eae33e5cc324189d72196557f8590635f2a99f68
BeliMackendy <belizaire.mackendy@gmail.com>
2022-08-07 22:07:23 -0400

    Showing the data with a RecyclerView

---
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TweetsAdapter.java
@@ -0,0 +1,70 @@
+package com.codepath.apps.restclienttemplate;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners;
+import com.codepath.apps.restclienttemplate.models.Tweet;
+
+import java.util.List;
+
+public class TweetsAdapter extends RecyclerView.Adapter<TweetsAdapter.ViewHolder> {
+    List<Tweet> tweets;
+
+    public TweetsAdapter(List<Tweet> tweets) {
+        this.tweets = tweets;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        View view = inflater.inflate(R.layout.item_tweet, parent, false);
+
+        // Return a new holder instance
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+        Tweet tweet = tweets.get(position);
+
+        holder.tvName.setText(tweet.user.name);
+        holder.tvScreenName.setText("@" + tweet.user.screenName);
+        Glide
+                .with(holder.itemView.getContext())
+                .load(tweet.user.profileImageUrl)
+                .transform(new RoundedCorners(100))
+                .into(holder.ivProfileImage);
+        holder.tvBody.setText("" + tweet.body);
+    }
+
+    @Override
+    public int getItemCount() {
+        return tweets.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        TextView tvName;
+        TextView tvScreenName;
+        TextView tvBody;
+        ImageView ivProfileImage;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            tvName = itemView.findViewById(R.id.tvName);
+            tvScreenName = itemView.findViewById(R.id.tvScreenName);
+            tvBody = itemView.findViewById(R.id.tvBody);
+            ivProfileImage = itemView.findViewById(R.id.ivProfileImage);
+        }
+    }
+}
